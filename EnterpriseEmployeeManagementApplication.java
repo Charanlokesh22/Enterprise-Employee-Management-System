d229d7a88b48fc0f6a6bfc5a66756f91fa0b5844
Charanlokesh22 <charanlokesh2918@gmail.com>
2026-02-03 21:00:04 +0530

    Create EnterpriseEmployeeManagementApplication.java

---
+++ EnterpriseEmployeeManagementApplication.java
@@ -0,0 +1,12 @@
+package com.company.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseEmployeeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseEmployeeManagementApplication.class, args);
+    }
+}
