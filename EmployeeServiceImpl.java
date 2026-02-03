03e24e873ea348b2b3e6e9253bcc2ea9afdef2ef
Charanlokesh22 <charanlokesh2918@gmail.com>
2026-02-03 21:00:49 +0530

    Create EmployeeServiceImpl.java

---
+++ EmployeeServiceImpl.java
@@ -0,0 +1,38 @@
+package com.company.ems.service;
+
+import com.company.ems.entity.Employee;
+import com.company.ems.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee not found"));
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
